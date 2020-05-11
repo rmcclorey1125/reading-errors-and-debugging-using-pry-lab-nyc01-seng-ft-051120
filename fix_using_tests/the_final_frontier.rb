6a531b5851c9f8a45f3440d9ac793c8a3c1cd99f
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-05-11 20:05:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,15 +1,17 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
-  "Captain's Log, star date #{star_date}."
+def state_log star_date
+  "Captain's Log, star_date #{star_date}."
 end
 
 def engage
-  puts state_log(date)
+  puts state_log
   date = generate_star_date
 end
+binding.pry
